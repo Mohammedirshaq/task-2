dad93a4fe8757644f2f917a74187f5d2a1e7d81d
Mohammedirshaq <82198906+Mohammedirshaq@users.noreply.github.com>
2025-02-17 19:37:02 +0530

    Update task.sh

--- task.sh
+++ task.sh
@@ -6,5 +6,4 @@ current_date=$(date "+%Y-%m-%d_%H-%M-%S")
 # Create a file with the name 'file_<current_date>'
 touch "file_$current_date"
 
-# Print the name of the created file
 echo "File 'file_$current_date' created."
